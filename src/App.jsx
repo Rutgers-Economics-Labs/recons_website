2008deea75c3ea6e004245ec05be225ef23739c6
Christopher Dsouza <cdgithub11@gmail.com>
2026-02-03 20:34:34 -0500

    Hashing instead of Browser 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 
 import Home from "./pages/Home";
@@ -16,7 +16,7 @@ import ReviewJournal from './pages/ReviewJournal';
 
 function App() {
   return (
-    <Router basename="/recons_website">
+    <Router>
       <div className="min-h-screen flex flex-col bg-gray-100">
         <Header />
         <main className="flex-grow">
